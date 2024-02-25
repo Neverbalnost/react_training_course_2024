b4bc2f91fcddc9a458a15f800feb9c773e2cb015
Elena Kogodeeva <elena_kogodeeva@epam.com>
2024-02-26 01:24:42 +0300

    Create search component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Counter from './components/counterComponent';
+import SearchForm from './components/searchForm';
 
 function App() {
   return (
     <div id='root'>
       <div className='counter'>
         <Counter value={0}></Counter>
+        <SearchForm value={'Random stuff about dinosaurs'} callback={(prompt) => alert(`WOOOOAAAAA!!! You were looking for "${prompt}"! Impressive!`)}></SearchForm>
       </div>
     </div>
   );
---
+++ src/components/searchForm.js
@@ -0,0 +1,16 @@
+import React, {useState} from "react";
+export default function SearchForm(props) {
+    const [searchPrompt, setSearchPrompt] = useState(props.value);
+
+    return (
+        <div className='search'>
+            <input className='search__input' value={searchPrompt} onKeyUp={(e) => {
+                if (e.code === 'Enter') {
+                    props.callback(searchPrompt);
+                }
+            }} onChange={e => setSearchPrompt(e.target.value)}></input>
+            <button className='search__button' onClick={() => props.callback(searchPrompt)}>Search!</button>
+        </div>
+    )
+
+}
